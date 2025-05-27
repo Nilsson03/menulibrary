e35f9585cb0131546b98e70f006a533ca7c77e90
Nilsson03 <nilsson_nicholas@mail.ru>
2025-05-27 19:19:28 +0300

    Added the parseSection() method to get CustomItem from configuration.

--- src/main/java/ru/nilsson03/library/menu/item/util/ItemUtil.java
+++ src/main/java/ru/nilsson03/library/menu/item/util/ItemUtil.java
@@ -1,9 +1,15 @@
 package ru.nilsson03.library.menu.item.util;
 
+import org.bukkit.configuration.ConfigurationSection;
+import ru.nilsson03.library.NPlugin;
 import ru.nilsson03.library.bukkit.item.builder.SkullItemBuilder;
 import ru.nilsson03.library.bukkit.item.builder.impl.UniversalSkullBuilder;
 import ru.nilsson03.library.bukkit.item.skull.SkullTextureHandler;
 import ru.nilsson03.library.bukkit.item.skull.factory.SkullHandlerFactory;
+import ru.nilsson03.library.bukkit.util.log.ConsoleLogger;
+import ru.nilsson03.library.menu.item.impl.CustomItem;
+
+import java.util.*;
 
 public class ItemUtil {
 
@@ -12,5 +18,22 @@ public class ItemUtil {
 
     public static SkullItemBuilder createHead(String url) {
         return builder.setSkinTexture(url);
+    }
+
+    public static List<CustomItem> parseSection(NPlugin plugin, ConfigurationSection section) {
+        Objects.requireNonNull(section, "Items section cannot be null");
+        List<CustomItem> customItems = new ArrayList<>();
+        for (String key : section.getKeys(false)) {
+            char symbol = key.charAt(0);
+            ConfigurationSection itemConfig = section.getConfigurationSection(key);
+            if (itemConfig == null) {
+                ConsoleLogger.warn(plugin, "Could not get item section for %s from configuration", key);
+                continue;
+            }
+            CustomItem item = new CustomItem(plugin, itemConfig);
+
+            customItems.add(item);
+        }
+        return customItems;
     }
 }
